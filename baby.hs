9231a5f2696e46663b331d667f93820ab49a6266
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-08-11 16:00:21 +0900

    introduce brittany and hlint

--- baby.hs
+++ baby.hs
@@ -1,11 +1,13 @@
-
-
-
 doubleMe x = x + x
 
-doubleUs x y = x *2 +y*2
+doubleUs :: Num a => a -> a -> a
+doubleUs x y = x * 2 + y * 2
 
-doubleSmallNumber x = if x > 100 then x else x *2
-doubleSmallNumber' x   = (if x > 100 then x else x *2)+1
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = if x > 100 then x else x * 2
 
+doubleSmallNumber' :: (Num a, Ord a) => a -> a
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+triangles :: Integer
 triangles = 3
