2a6943706d3deeb544591ec776ac76c3e07de6dc
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-08-12 11:03:58 +0900

    IO etc.

--- baby.hs
+++ baby.hs
@@ -1,5 +1,8 @@
 import           Control.Arrow                  ( Arrow(arr) )
+import           Control.Monad                  ( when )
 import           Distribution.Simple.Utils      ( xargs )
+
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
 doubleUs :: Num a => a -> a -> a
@@ -66,3 +69,86 @@ elem' :: (Eq a) => a -> [a] -> Bool
 elem' a [] = False
 elem' a (x : xs) | a == x    = True
                  | otherwise = a `elem` xs
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort []       = []
+quickSort (x : xs) = quickSort smaller ++ [x] ++ quickSort larger
+ where
+  smaller = [ a | a <- xs, a <= x ]
+  larger  = [ a | a <- xs, x < a ]
+
+
+quickSort' :: (Ord a) => [a] -> [a]
+quickSort' [] = []
+quickSort' (x : xs) =
+  let smaller = [ a | a <- xs, a <= x ]
+      larger  = [ a | a <- xs, x < a ]
+  in  quickSort smaller ++ [x] ++ quickSort larger
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ []       _        = []
+zipWith' _ _        []       = []
+zipWith' f (x : xs) (y : xy) = f x y : zipWith' f xs xy
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain x | even x    = x : chain (x `div` 2)
+        | otherwise = x : chain (x * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1 .. 100]))
+  where isLong arr = 15 < length arr
+
+numLongChains' :: Int
+numLongChains' =
+  length (filter (\arr -> 15 < length arr) (map chain [1 .. 100]))
+
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' f a []       = a
+foldl' f a (x : xs) = foldl f (f a x) xs
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f a []       = a
+foldr' f a (x : xs) = f x (foldr f a xs)
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' a = foldr (\x acc -> acc && x == a) False
+
+reverse'' :: [a] -> [a]
+-- reverse'' = foldr (\x acc -> acc ++ [x]) []  -- slower
+reverse'' = foldl (flip (:)) []
+
+last' :: [a] -> a
+last' = foldl1 (\acc x -> x)
+
+last'' :: [a] -> a
+last'' = foldr1 (\x acc -> acc)
+
+sqrtSums :: Int
+sqrtSums =
+  length (takeWhile (< 1000) (scanl1 (\acc x -> acc + sqrt x) [1 ..])) + 1
+
+
+main :: IO ()
+main = do
+  when True $ do
+    putStrLn "hello world."
+
+putStr' :: String -> IO ()
+putStr' []       = return ()
+putStr' (x : xs) = do
+  putChar x
+  putStr xs
+
+putStrLn' :: String -> IO ()
+putStrLn' xs = do
+  putStr' xs
+  putChar '\n'
+  return ()
+
+
+
+
+
+
+
---
+++ chapter9/capslocker.hs
@@ -0,0 +1,80 @@
+import           Control.Monad
+import           Data.Char                      ( toUpper )
+import           System.IO                      ( IOMode(ReadMode)
+                                                , hClose
+                                                , hGetContents
+                                                , openFile
+                                                , readFile
+                                                , withFile
+                                                )
+main :: IO ()
+----------------------------------------
+-- with getLine
+
+-- main = forever $ do
+--   l <- getLine
+--   putStrLn $ map toUpper l
+
+----------------------------------------
+-- with getContents
+
+-- main = do
+--   contents <- getContents
+--   putStrLn $ map toUpper contents
+
+----------------------------------------
+-- with shortLinesOnly
+
+-- main = do
+--   contents <- getContents
+--   putStrLn $ shortLinesOnly contents
+
+-- shortLinesOnly :: String -> String
+-- shortLinesOnly = unlines . filter (\l -> length l < 20) . lines
+
+----------------------------------------
+-- with interact
+
+-- main = interact shortLinesOnly
+
+-- shortLinesOnly :: String -> String
+-- shortLinesOnly = unlines . filter (\l -> length l < 20) . lines
+
+----------------------------------------
+-- palindrome
+
+-- main = interact respondPalindromes
+
+-- respondPalindromes :: String -> String
+-- respondPalindromes =
+--   unlines
+--     . map (\l -> if isPal (l) then "palindrome" else "not a palindrome")
+--     . lines
+
+-- isPal :: String -> Bool
+-- isPal "" = False
+-- isPal xs = xs == reverse xs
+
+----------------------------------------
+-- with openFile
+
+-- main = do
+--   handle   <- openFile "chapter9/haiku.txt" ReadMode
+--   contents <- hGetContents handle
+--   putStrLn $ map toUpper contents
+--   hClose handle
+
+----------------------------------------
+-- with withFile
+
+-- main = do
+--   withFile "chapter9/haiku.txt" ReadMode $ \handle -> do
+--     contents <- hGetContents handle
+--     putStrLn $ map toUpper contents
+
+----------------------------------------
+-- with readFile
+
+main = do
+  contents <- readFile "chapter9/haiku.txt"
+  putStrLn $ map toUpper contents
